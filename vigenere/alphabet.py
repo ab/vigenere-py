0fd06638afb27eb0ba17b661a0d4f283f30cd023
Andy Brody <git@abrody.com>
2023-10-23 15:08:11 -0400

    Consolidate encrypt/decrypt into a single func.
    - Refactor encrypt/decrypt into a single function that takes a parameter
      for whether it's doing addition or subtraction.
    - Change input parsing so that we strip out passthrough characters from
      key before starting.
    - Improve calculation of needed key length so that we allow extra
      passthrough chars to be present in the plaintext/ciphertext without
      needlessly requiring a longer key.

--- vigenere/alphabet.py
+++ vigenere/alphabet.py
@@ -4,29 +4,41 @@ import string
 
 @dataclasses.dataclass
 class Alphabet:
+    name: str
     chars: str
     passthrough: set[str]
     chars_dict: dict[str, int] = dataclasses.field(init=False)
 
     def __post_init__(self) -> None:
         self.chars_dict = {v: i for i, v in enumerate(self.chars)}
+        self._passthrough_trans = str.maketrans({c: None for c in self.passthrough})
+
+    def remove_passthrough(self, text: str) -> str:
+        """
+        Return the provided text with passthrough characters removed.
+        """
+        return text.translate(self._passthrough_trans)
 
 
 ALPHABET_PRINTABLE = Alphabet(
+    name="printable",
     chars=" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",  # noqa: E501
     passthrough={"\t", "\n", "\v", "\f", "\r"},
 )
 
 ALPHABET_LETTERS_ONLY = Alphabet(
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    name="letters",
+    chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     passthrough=set(string.punctuation + string.whitespace),
 )
 ALPHABET_ALPHANUMERIC_UPPER = Alphabet(
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    name="alphanumeric-upper",
+    chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
     passthrough=set(string.punctuation + string.whitespace),
 )
 ALPHABET_ALPHANUMERIC_MIXED = Alphabet(
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    name="alphanumeric",
+    chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
     passthrough=set(string.punctuation + string.whitespace),
 )
 
--- vigenere/cipher.py
+++ vigenere/cipher.py
@@ -1,5 +1,6 @@
+import operator
 import secrets
-from typing import Optional, TextIO
+from typing import Callable, Optional, TextIO
 
 from .alphabet import get_alphabet
 from .errors import CipherError, CLIError
@@ -28,61 +29,39 @@ class Cipher:
         if not key:
             raise ValueError("Empty key")
 
-        self.key = key
-
         self.alphabet = get_alphabet(name=alphabet_name)
 
+        self.key = self.alphabet.remove_passthrough(key)
+
     def encrypt(self, text: str) -> str:
-        if text is None:
-            raise ValueError("Must provide text")
-
-        if len(self.key) < len(text):
-            raise CipherError("Key is shorter than plaintext")
-
-        output = ""
-
-        iter_in = iter(text)
-        iter_key = iter(self.key)
-
-        for c, k in zip(iter_in, iter_key):
-            # pass through certain plaintext without consuming key
-            while c in self.alphabet.passthrough:
-                output += c
-                try:
-                    c = next(iter_in)
-                except StopIteration:
-                    return output
-
-            try:
-                c_int = self.alphabet.chars_dict[c]
-            except KeyError:
-                raise CipherError(f"Invalid character in plaintext: {c!r}")
-
-            try:
-                k_int = self.alphabet.chars_dict[k]
-            except KeyError:
-                raise CipherError(f"Invalid character in key: {k!r}")
-
-            o_int = (c_int + k_int) % len(self.alphabet.chars)
-            o_chr = self.alphabet.chars[o_int]
-
-            output += o_chr
-
-        return output
+        """Encrypt provided text."""
+        return self._crypt(text=text, op=operator.add, input_label="plaintext")
 
     def decrypt(self, text: str) -> str:
+        """Decrypt provided text."""
+        return self._crypt(text=text, op=operator.sub, input_label="ciphertext")
+
+    def _crypt(self, text: str, op: Callable[[int, int], int], input_label: str) -> str:
+        """
+        Generic function handling encrypt and decrypt
+        """
         if text is None:
             raise ValueError("Must provide text")
 
-        if len(self.key) < len(text):
-            raise CipherError("Key is shorter than ciphertext")
+        if len(self.key) < len(self.alphabet.remove_passthrough(text)):
+            raise CipherError(f"Key is shorter than {input_label}")
 
         output = ""
 
         iter_in = iter(text)
         iter_key = iter(self.key)
 
-        for c, k in zip(iter_in, iter_key):
+        while True:
+            try:
+                c = next(iter_in)
+            except StopIteration:
+                return output
+
             # pass through certain text without consuming key
             while c in self.alphabet.passthrough:
                 output += c
@@ -92,16 +71,21 @@ class Cipher:
                     return output
 
             try:
+                k = next(iter_key)
+            except StopIteration:
+                raise CipherError(f"Unexpected key is shorter than {input_label}")
+
+            try:
                 c_int = self.alphabet.chars_dict[c]
             except KeyError:
-                raise CipherError(f"Invalid character in ciphertext: {c!r}")
+                raise CipherError(f"Invalid character in {input_label}: {c!r}")
 
             try:
                 k_int = self.alphabet.chars_dict[k]
             except KeyError:
                 raise CipherError(f"Invalid character in key: {k!r}")
 
-            o_int = (c_int - k_int) % len(self.alphabet.chars)
+            o_int = op(c_int, k_int) % len(self.alphabet.chars)
             o_chr = self.alphabet.chars[o_int]
 
             output += o_chr
