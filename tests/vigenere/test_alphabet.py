cc2cafe3dce3839b78e58b872f1b19fa1d9242a9
Andy Brody <git@abrody.com>
2024-07-02 22:47:03 -0400

    Add tests for new alphabet functions.

--- tests/vigenere/test_alphabet.py
+++ tests/vigenere/test_alphabet.py
@@ -6,11 +6,13 @@ import pytest
 from vigenere.alphabet import (
     Alphabet,
     get_alphabet,
+    ALPHABET_DECIMAL,
     ALPHABET_PRINTABLE,
     ALPHABET_LETTERS_ONLY,
     ALPHABET_ALPHANUMERIC_UPPER,
     ALPHABET_ALPHANUMERIC_MIXED,
 )
+from vigenere.errors import InputError
 
 
 def test_get_alphabet():
@@ -24,10 +26,15 @@ def test_get_alphabet():
     assert len(printable.chars) == 95
     assert len(printable.passthrough) == 5
 
+    assert get_alphabet("100") == ALPHABET_DECIMAL
+    assert get_alphabet("decimal") == ALPHABET_DECIMAL
     assert get_alphabet("letters") == ALPHABET_LETTERS_ONLY
     assert get_alphabet("upper") == ALPHABET_LETTERS_ONLY
     assert get_alphabet("alphanumeric") == ALPHABET_ALPHANUMERIC_MIXED
     assert get_alphabet("alphanumeric-upper") == ALPHABET_ALPHANUMERIC_UPPER
+
+    with pytest.raises(ValueError, match="name must be str"):
+        get_alphabet(name=123)
 
 
 def test_alphabet():
@@ -64,6 +71,11 @@ def test_alphabet():
         "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
     )
 
+    assert ALPHABET_DECIMAL.decimal
+    assert ALPHABET_DECIMAL.ansi_spaces is False
+    assert ALPHABET_PRINTABLE.ansi_spaces is True
+    assert ALPHABET_LETTERS_ONLY.ansi_spaces is False
+
 
 def test_generate_key_mock(mocker):
     spy = mocker.spy(secrets, "choice")
@@ -74,6 +86,17 @@ def test_generate_key_mock(mocker):
 
     # make sure we called secrets.choice
     assert spy.call_args_list == [mocker.call("ABCDEFGHIJKLMNOPQRSTUVWXYZ")] * 5
+
+
+def test_generate_key_decimal(mocker):
+    mock = mocker.patch.object(secrets, "choice")
+    mock.side_effect = ["R", "a", "n", "d"]
+
+    key = ALPHABET_DECIMAL.generate_key(length=4)
+
+    assert mock.call_args_list == [mocker.call(ALPHABET_DECIMAL.chars)] * 4
+
+    assert key == "55 70 83 73"
 
 
 def test_generate_key():
@@ -92,3 +115,72 @@ def test_generate_key():
     key3 = ALPHABET_LETTERS_ONLY.generate_key(10)
     assert len(key3) == 10
     assert key != key3
+
+
+ENCODE_FIXTURES = [
+    ("decimal", "FOO!", "43 52 52 06"),
+    ("decimal", "Hello, World!\n", "45 74 81 81 84 17 05 60 84 87 81 73 06 02"),
+    ("decimal", "Null: \0 OK", "51 90 81 81 31 05 00 05 52 48"),
+    ("letters", "ABCZ", "00 01 02 25"),
+    ("printable", " Az.!", "00 33 90 14 01"),
+]
+
+
+@pytest.mark.parametrize("alphabet,text,encoded", ENCODE_FIXTURES)
+def test_decimal_encode_decode(alphabet: str, text: str, encoded: str) -> None:
+    alpha = get_alphabet(alphabet)
+
+    assert alpha.decimal_encode(text) == encoded
+
+    assert alpha.decimal_decode(encoded) == text
+
+
+def test_decimal_wrap() -> None:
+    alpha = ALPHABET_DECIMAL
+
+    text = "ABC" * 25
+    encoded = """
+38 39 40 38 39 40 38 39 40 38 39 40 38 39 40 38 39 40 38 39
+40 38 39 40 38 39 40 38 39 40 38 39 40 38 39 40 38 39 40 38
+39 40 38 39 40 38 39 40 38 39 40 38 39 40 38 39 40 38 39 40
+38 39 40 38 39 40 38 39 40 38 39 40 38 39 40
+""".strip()
+
+    assert alpha.decimal_encode(text) == encoded
+
+    nowrap = ("38 39 40 " * 25).rstrip()
+    assert alpha.decimal_encode(text, wrap=0) == nowrap
+
+    assert alpha.decimal_decode(encoded) == text
+    assert alpha.decimal_decode(nowrap) == text
+
+
+@pytest.mark.parametrize(
+    "alphabet,text,err_regex",
+    [
+        ("decimal", "¿", "Invalid input char '¿' for alphabet 'decimal'"),
+        ("letters", "ABCz", "Invalid input char 'z' for alphabet 'letters'"),
+        ("letters", "A BEAR", "Invalid input char ' ' for alphabet 'letters'"),
+    ],
+)
+def test_decimal_encode_errors(alphabet: str, text: str, err_regex: str) -> None:
+    alpha = get_alphabet(alphabet)
+
+    with pytest.raises(InputError, match=err_regex):
+        alpha.decimal_encode(text)
+
+
+@pytest.mark.parametrize(
+    "alphabet,text,err_regex",
+    [
+        ("decimal", "12 abc", "Invalid decimal input: 'abc'"),
+        ("decimal", "12 -45", "Negative numbers are invalid: -45"),
+        ("decimal", "10 200", "Invalid input for alphabet: '200' not in 0..99"),
+        ("letters", "10 50", "Invalid input for alphabet: '50' not in 0..25"),
+    ],
+)
+def test_decimal_decode_errors(alphabet: str, text: str, err_regex: str) -> None:
+    alpha = get_alphabet(alphabet)
+
+    with pytest.raises(InputError, match=err_regex):
+        alpha.decimal_decode(text)
